1b41047e9fad76159ee2d880c7a5e018a70d08e2
Muaz Hussain <muaz.self@gmail.com>
2023-05-24 18:45:00 +0600

    INITIAL COMMIT
    Basic setup for ecommerce project.

---
+++ README.md
@@ -0,0 +1,4 @@
+# ecommerce_project_ostad
+
+Ostad E-Commerce Project.
+Batch - 02.
---
+++ android/app/src/main/kotlin/com/example/ecommerce_project_ostad/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ecommerce_project_ostad
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
